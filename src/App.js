41e31c0ff839796793344f883075ceecf9ddd26b
GavinG60 <gwg08270@uga.edu>
2022-11-14 01:09:13 -0500

    building front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>Athens Restaurant Finder</h1>
+        <p>Sign in</p>
+      </div>
+
+      <div className="App-body">
+
+        <div className="Filters">
+          <p>Filters</p>
+        </div>
+
+        <div className="refresh">
+          <a href="#">
+            <img src="refresh.jpg" alt="refresh image"></img>
+          </a>
+        </div>
+
+        <div className="Change-components">
+          <a href="#" id="button1">Add/Remove Components</a>
+        </div>
+
+        <div className="select" id="prices">
+          <select name="standard-select" id="price-select">
+            <option value="">--Select a price range--</option>
+            <option value="$">$</option>
+            <option value="$$">$$</option>
+            <option value="$$$">$$$</option>
+          </select>
+        </div>
+
+        <div className="select" id="food">
+          <select name="standard-select" id="food-select">
+            <option value="">--Select a food type--</option>
+            <option value="Mexican">Mexican</option>
+            <option value="Italian">Italian</option>
+            <option value="Indian">Indian</option>
+            <option value="Greek">Greek</option>
+            <option value="Chinese">Chinese</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+
+      </div>
     </div>
   );
 }

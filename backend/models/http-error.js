4a49ac673ced27b7d0fd42e7cf5dc82bbadab136
NolbertChastain <79341405+NolbertChastain@users.noreply.github.com>
2022-11-20 20:46:08 -0500

    created Mongoose Schema & model & attempted to connect to mongDB

---
+++ backend/models/http-error.js
@@ -0,0 +1,7 @@
+class HttpError extends Error {
+    constructor(message, errorCode) {
+        super(message);             // message property
+        this.code = errorCode;      // code property
+    }
+}
+module.exports = HttpError;
